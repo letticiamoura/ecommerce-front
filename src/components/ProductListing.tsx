486ff2da767b08fba62c99d3410dadb4dc05f857
letticiamoura <leticiamouralopes5@gmail.com>
2024-07-24 12:11:06 -0300

    feat: Alterandi logica ProductsListing

--- src/components/ProductListing.tsx
+++ src/components/ProductListing.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ProductCard from "./ProductCard";
 
+import nikeBlue from "../assets/products/nike-blue.png";
 import nikeG from "../assets/products/nike-yellow.png";
+import nikeB from "../assets/products/nike-black.png";
 
 interface Product {
     category: string;
@@ -11,32 +13,29 @@ interface Product {
     priceDiscount?: number;
 }
 
-const products = [
+const products: Product[] = [
     { category: "Tênis", name: "K-swiss V8 - Masculino", image: nikeG, price: 49.9, priceDiscount: 20 },
+    { category: "Tênis", name: "Nike - Feminino", image: nikeBlue, price: 50.9, priceDiscount: 20 },
+    { category: "Tênis", name: "Puma - Masculino", image: nikeB, price: 50.9, priceDiscount: 20 }
 ]
-
-const testProduct: Product = {
-    category: "Tênis",
-    name: "K-swiss V8 - Masculino",
-    image: nikeG,
-    price: 49.9,
-    priceDiscount: 20
-};
 
 const ProductListing: React.FC = () => {
     return(
         <div className='flex justify-center box-border flex-wrap md:w-[80vw] m-auto gap-x-6 gap-y-8'>
-            {/* Para teste: gera 9 cópias do testProcuts */}
-            {[...Array(8)].map((_, index) => (
-                <ProductCard
-                    key={index}
-                    category={testProduct.category}
-                    name={testProduct.name}
-                    image={testProduct.image}
-                    price={testProduct.price}
-                    priceDiscount={testProduct.priceDiscount}
-                />
-            ))}
+        
+        {Array.from({ length: 8 }).map((_, index) => {
+                const product = products[index % products.length];
+                return (
+                    <ProductCard
+                        key={index}
+                        category={product.category}
+                        name={product.name}
+                        image={product.image}
+                        price={product.price}
+                        priceDiscount={product.priceDiscount}
+                    />
+                );
+            })}
         </div>
     );
 };
