b985a7e3eefbdc2ac98c05c9e961321d9dbe36f9
letticiamoura <leticiamouralopes5@gmail.com>
2024-07-28 15:15:20 -0300

    position

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -18,7 +18,7 @@ const ProductCard = ({ category, image, name, price, priceDiscount }: ProductCar
 
     <Link to="ecommerce-front/product" onClick={handleClick}>
 
-        <div className='w-[40vw] sm:w-[30vw] md:w-[17vw] pt-10'>
+        <div className='w-[39vw] sm:w-[30vw] md:w-[17vw] pt-10'>
 
           <div className="h-[30vh] md:h-[35vh] flex justify-center items-center bg-white">
             <img className='h-auto w-[39vw] md:w-[14vw] object-cover pb-6 box-border -rotate-12' src={image} alt={name} />
