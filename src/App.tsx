857cae7b29c949592a94101b549d9361a8127ee6
letticiamoura <leticiamouralopes5@gmail.com>
2024-08-09 12:01:28 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { lazy, Suspense } from "react";
 import ErrorPage from "./pages/ErrorPage";
 import HomePage from "./pages/HomePage";
 import Register from "./pages/Register";
@@ -6,27 +7,31 @@ import RegisterForm from "./pages/RegisterForm";
 import Login from "./pages/Login";
 import ProductPage from "./pages/ProductPage";
 import OrderPage from "./pages/OrderPage";
-import ProductViewPage from "./pages/ProductViewPage";
+
+const ProductViewPage = lazy(() => import("./pages/ProductViewPage"));
 
 export default function App() {
 
   return (
-      <BrowserRouter>
+
+    <BrowserRouter>
+
+      <Suspense fallback={<div>Loading...</div>}>
 
         <Routes>
-          <Route path="/ecommerce-front/" element={ <HomePage />} />
-          <Route path="*" element={ <ErrorPage />} />
-          <Route path="ecommerce-front/login" element={ <Login />} />
-          <Route path="ecommerce-front/myproducts" element={ <OrderPage />} />
-          <Route path="ecommerce-front/product/:id" element={ <ProductViewPage />} />
-          <Route path="ecommerce-front/product/*" element={ <ProductViewPage />} />
-          <Route path="ecommerce-front/register" element={ < Register /> } />
-          <Route path="ecommerce-front/products" element={ <ProductPage />} />
-          <Route path="ecommerce-front/register/registerform" element={ <RegisterForm/> } />
-      </Routes>
-    
+          <Route path="*" element={<ErrorPage />} />
+          <Route path="/ecommerce-front/" element={<HomePage />} />
+          <Route path="/ecommerce-front/login" element={<Login />} />
+          <Route path="/ecommerce-front/register" element={<Register />} />
+          <Route path="/ecommerce-front/products" element={<ProductPage />} />
+          <Route path="/ecommerce-front/myproducts" element={<OrderPage />} />
+          <Route path="/ecommerce-front/product/:id" element={<ProductViewPage />} />
+          <Route path="/ecommerce-front/register/registerform" element={<RegisterForm />} />
+        </Routes>
+
+      </Suspense>
+
     </BrowserRouter>
 
-  )
-
+  );
 }
